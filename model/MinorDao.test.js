3bba1c3bc0190ee4ab8e74c88b182fb73ce5e9e4
lesliekim1 <lskim@loyola.edu>
2025-10-18 14:03:03 -0400

    rename newdata to newData variable in tests #4

--- model/MinorDao.test.js
+++ model/MinorDao.test.js
@@ -27,12 +27,9 @@ beforeEach(async function() {
  * Create new minor test.
  */
 test('Create new minor', async function() {
-    let newdata = { 
-        name: 'Child 1', 
-        date_of_birth: '2012-05-10', 
-        parent_id: '68f321cef24b117cb0cc4a11' 
-    };
-    let created = await dao.create(newdata);
+    let newData = {name: 'Child 1', date_of_birth: '2012-05-10', parent_id: '68f321cef24b117cb0cc4a11',
+        team_id: '68f321cef200117cb0cc4a11'};
+    let created = await dao.create(newData);
     let found = await dao.read(created._id);
     expect(created._id).not.toBeNull(); 
     expect(found.name).toBe(created.name);
@@ -42,12 +39,9 @@ test('Create new minor', async function() {
  * Delete minor test.
  */
 test('Delete minor', async function() {
-    let newdata = { 
-        name: 'Child 1', 
-        date_of_birth: '2012-05-10', 
-        parent_id: '68f321cef24b117cb0cc4a11' 
-    };
-    let created = await dao.create(newdata); 
+    let newData = {name: 'Child 1', date_of_birth: '2012-05-10', parent_id: '68f321cef24b117cb0cc4a11',
+        team_id: '68f321cef200117cb0cc4a11'};
+    let created = await dao.create(newData); 
     let deleted = await dao.del(created._id);
     let found = await dao.read(created._id); 
     expect(found).toBeNull();
@@ -58,12 +52,15 @@ test('Delete minor', async function() {
  * Read all minor documents test.
  */
 test('Read All', async function() {
-    let newdata1 = { name: 'Minor 1', date_of_birth: '2011-04-20', parent_id: '68f321cef24b117cb0cc4a11' };
-    let newdata2 = { name: 'Minor 2', date_of_birth: '2013-08-15', parent_id: '68f321cef23b117cb0cc4a11' };
-    let newdata3 = { name: 'Minor 3', date_of_birth: '2014-11-25', parent_id: '68f321cef24b118cb0cc4a11' };
-    await dao.create(newdata1);
-    await dao.create(newdata2);
-    await dao.create(newdata3);
+    let newData1 = {name: 'Minor 1', date_of_birth: '2011-04-20', parent_id: '68f321cef24b117cb0cc4a11', 
+        team_id: '68f321cef200117cb0cc4a11'};
+    let newData2 = {name: 'Minor 2', date_of_birth: '2013-08-15', parent_id: '68f321cef23b117cb0cc4a11', 
+        team_id: '68f321cef200117cb0cc4a12'};
+    let newData3 = {name: 'Minor 3', date_of_birth: '2014-11-25', parent_id: '68f321cef24b118cb0cc4a11', 
+        team_id: '68f321cef200117cb0cc4a13'};
+    await dao.create(newData1);
+    await dao.create(newData2);
+    await dao.create(newData3);
     let lstMinors = await dao.readAll();
     expect(lstMinors.length).toBe(3);
     expect(lstMinors[0].name).toBe('Minor 1');
@@ -73,13 +70,10 @@ test('Read All', async function() {
  * Find minor by name test.
  */
 test('Find Minor by name', async function() {
-    let newdata = { 
-        name: 'Minor 1', 
-        date_of_birth: '2010-06-12', 
-        parent_id: '68f321cef24b117cb0cc4a11' 
-    };
-    let created = await dao.create(newdata); 
-    let found = await dao.findByName(newdata.name);
+let newData1 = {name: 'Minor 1', date_of_birth: '2011-04-20', parent_id: '68f321cef24b117cb0cc4a11', 
+    team_id: '68f321cef200117cb0cc4a11'};
+    let created = await dao.create(newData1); 
+    let found = await dao.findByName(newData1.name);
     expect(found).not.toBeNull();
     expect(found._id).toEqual(created._id);
     expect(found.name).toEqual(created.name);
--- model/UserDao.test.js
+++ model/UserDao.test.js
@@ -27,9 +27,9 @@ beforeEach(async function() {
  * Create new user test.
  */
 test('Create new user',async function() {
-    let newdata = {username: 'test', password: '12345678', email: 'test@gmail.com', 
+    let newData = {username: 'test', password: '12345678', email: 'test@gmail.com', 
         phone: '1230001234', date_of_birth: '10/17/2025', name: 'First Last', approve: false};
-    let created = await dao.create(newdata);
+    let created = await dao.create(newData);
     let found = await dao.read(created._id);
     expect(created._id).not.toBeNull(); 
     expect(created.login).toBe(found.login);
@@ -39,9 +39,9 @@ test('Create new user',async function() {
  * Delete user test.
  */
 test('Delete user', async function() {
-    let newdata = {username: 'test', password: '12345678', email: 'test@gmail.com', 
+    let newData = {username: 'test', password: '12345678', email: 'test@gmail.com', 
         phone: '1230001234', date_of_birth: '10/17/2025', name: 'First Last', approve: false};
-    let created = await dao.create(newdata); 
+    let created = await dao.create(newData); 
     let deleted = await dao.del(created._id);
     let found = await dao.read(created._id); 
     expect(found).toBeNull();
@@ -52,15 +52,15 @@ test('Delete user', async function() {
  * Read all user documents test.
  */
 test('Read All', async function() {
-    let newdata1 = {username: 'test1', password: '12345678', email: 'test1@gmail.com', 
+    let newData1 = {username: 'test1', password: '12345678', email: 'test1@gmail.com', 
         phone: '1230001234', date_of_birth: '10/17/2025', name: 'First Last', approve: false};
-    let newdata2 = {username: 'test2', password: '123456789', email: 'test2@gmail.com', 
+    let newData2 = {username: 'test2', password: '123456789', email: 'test2@gmail.com', 
         phone: '1230001234', date_of_birth: '10/17/2025', name: 'First Last', approve: false};
-    let newdata3 = {username: 'test3', password: '12345678910', email: 'test3@gmail.com', 
+    let newData3 = {username: 'test3', password: '12345678910', email: 'test3@gmail.com', 
         phone: '1230001234', date_of_birth: '10/17/2025', name: 'First Last', approve: false};
-    await dao.create(newdata1);
-    await dao.create(newdata2);
-    await dao.create(newdata3);
+    await dao.create(newData1);
+    await dao.create(newData2);
+    await dao.create(newData3);
     let lstUsers = await dao.readAll();
     expect(lstUsers.length).toBe(3);
     expect(lstUsers[0].username).toBe('test1');
@@ -70,10 +70,10 @@ test('Read All', async function() {
  * Find login user test.
  */
 test('Find Login user', async function() {
-    let newdata = {username: 'test1', password: '12345678', email: 'test1@gmail.com', 
+    let newData = {username: 'test1', password: '12345678', email: 'test1@gmail.com', 
         phone: '1230001234', date_of_birth: '10/17/2025', name: 'First Last', approve: false};
-    let created = await dao.create(newdata); 
-    let logged = await dao.findLogin(newdata.username);
+    let created = await dao.create(newData); 
+    let logged = await dao.findLogin(newData.username);
     expect(logged).not.toBeNull();
     expect(logged._id).toEqual(created._id);
     expect(logged.username).toEqual(created.username);
