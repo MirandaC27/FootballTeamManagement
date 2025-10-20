23354b21c4fddbda3726f00a60e0e134021a55ad
chloemiranda <cmmiranda@loyola.edu>
2025-10-20 00:56:09 -0400

    start of calendar array

---
+++ calendar.js
@@ -0,0 +1,9 @@
+function calendarArray{
+    //12 is months in a year, make constant later
+    let arr = new Array(12);
+    for(let x = 0; x < arr.length; x++){
+        arr[x]  = new Array(6);
+    }
+
+    for
+}
