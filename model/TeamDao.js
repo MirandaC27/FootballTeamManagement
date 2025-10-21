5185b7ca57f87182974677d32aa1d8a74707b4be
asdominguez <asdominguez@loyola.edu>
2025-10-21 17:15:33 -0400

    added team database #5 #6

---
+++ model/TeamDao.js
@@ -0,0 +1,100 @@
+const mongoose = require('mongoose');
+
+// Team structure schema.
+const teamSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    wins: {
+        type: Number,
+        required: true,
+    },
+    losses: {
+        type: Number,
+        required: true,
+    },
+    priorSeasonWins: {
+        type: Number,
+        required: true,
+    },
+    priorSeasonLosses: {
+        type: Number,
+        required: true,
+    },
+    playoffSeed: {
+        type: Number,
+        required: false,
+    }
+}
+);
+
+const teamModel = mongoose.model('team', teamSchema);
+
+// for testing purpose only:
+/**
+ * Read and return all team documents from the database.
+ */
+async function readAll() {
+  return await teamModel.find();
+}
+
+/**
+ * Read and return a single team document by its ID.
+ * @param {*} id document id
+ * @returns team model object if found
+ */
+async function read(id) {
+  return await teamModel.findById(id);
+}
+
+/**
+ * Create and save a new team document in the database.
+ * @param {*} newTeam new team document
+ * @returns new object
+ */
+async function create(newTeam) {
+  const team = new teamModel(newTeam);
+  await team.save();
+  return team;
+}
+
+/**
+ * Delete a single team document by its ID.
+ * @param {*} id team document id
+ * @returns deleted object if found
+ */
+async function del(id) {
+  return await teamModel.findByIdAndDelete(id);
+}
+
+/**
+ * Delete all team documents in database.
+ */
+async function deleteAll() {
+  await teamModel.deleteMany();
+}
+
+/**
+ * Find a team document based on their team name.
+ * @param {*} teamName team name to search for
+ * @returns object if found or null if not
+ */
+async function findTeam(teamName) {
+  return await teamModel.findOne({ teamName });
+}
+
+module.exports = {
+  create,
+  read,
+  readAll,
+  del,
+  deleteAll,
+  findTeam,
+  teamModel,
+};
---
+++ model/TeamDao.test.js
@@ -0,0 +1,88 @@
+const dbcon = require('./DbConnection');
+const dao = require('./TeamDao');
+
+/**
+ * Executed once before all tests
+ */
+beforeAll(async function() { 
+    dbcon.connect('test');
+});
+
+/**
+ *  Executed once after all tests have ran
+ */
+afterAll(async function() { 
+    await dao.deleteAll();
+    dbcon.disconnect();
+});
+
+/**
+ * Executed before each test
+ */
+beforeEach(async function() {
+    await dao.deleteAll();
+});
+
+/**
+ * Create new user test.
+ */
+test('Create new team',async function() {
+    let newData = {name: 'test', location: 'baltimore', wins: 2, 
+        losses: 1, priorSeasonWins: 13, priorSeasonLosses: 5};
+    let created = await dao.create(newData);
+    let found = await dao.read(created._id);
+    expect(created._id).not.toBeNull(); 
+    expect(created.login).toBe(found.login);
+});
+
+/**
+ * Delete team test.
+ */
+test('Delete team', async function() {
+    let newData = {name: 'test', location: 'baltimore', wins: 2, 
+        losses: 1, priorSeasonWins: 13, priorSeasonLosses: 5};
+    let created = await dao.create(newData); 
+    let deleted = await dao.del(created._id);
+    let found = await dao.read(created._id); 
+    expect(found).toBeNull();
+    expect(deleted._id).toEqual(created._id); 
+});
+
+/**
+ * Read all team documents test.
+ */
+test('Read All', async function() {
+    let newData = {name: 'lions', location: 'baltimore', wins: 2, 
+        losses: 1, priorSeasonWins: 13, priorSeasonLosses: 5};
+    let newData2 = {name: 'tigers', location: 'chicago', wins: 0, 
+        losses: 3, priorSeasonWins: 9, priorSeasonLosses: 9};
+    let newData3 = {name: 'bears', location: 'miami', wins: 2, 
+        losses: 1, priorSeasonWins: 3, priorSeasonLosses: 15};
+    await dao.create(newData);
+    await dao.create(newData2);
+    await dao.create(newData3);
+    let lstUsers = await dao.readAll();
+    expect(lstUsers.length).toBe(3);
+    expect(lstUsers[0].name).toBe('lions');
+});
+
+/**
+ * Find team test.
+ */
+test('Find team', async function() {
+    let newData = {name: 'lions', location: 'baltimore', wins: 2, 
+        losses: 1, priorSeasonWins: 13, priorSeasonLosses: 5};
+    let created = await dao.create(newData); 
+    let logged = await dao.findTeam(newData.username);
+    expect(logged).not.toBeNull();
+    expect(logged._id).toEqual(created._id);
+    expect(logged.username).toEqual(created.username);
+});
+
+/**
+ * Team not found test.
+ */
+test('Team not found', async function() {
+    let badLogged = await dao.findTeam("not a team"); 
+    expect(badLogged).toBeNull();
+});
