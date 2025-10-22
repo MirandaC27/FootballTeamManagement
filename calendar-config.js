a4fe7976f023f11c2cd628ed38b5fa5013ec3cdf
chloemiranda <cmmiranda@loyola.edu>
2025-10-22 01:23:15 -0400

    added html and css things for calendar. need to fix backend

--- calendar-config.js
+++ calendar-config.js
@@ -3,7 +3,7 @@
 function generateMonth (year, month){
     const monthArr = Array.from({ length: 6}, () => new Array(7).fill(""));
     const startDayInWeek = new Date(year, month, 1).getDay();
-    const monthLong = new Date(year, month, 1), getDay();
+    const monthLong = new Date(year, month+1, 0).getDay();
     
     let counter = 1;
     let started = false;
@@ -27,5 +27,19 @@ function generateMonth (year, month){
     return monthArr
 }
 
+function calendarArray(year, monthIndex = null){
+
+    if(monthArray != null){
+        return generateMonth(year, monthIndex);
+    }
+
+    else{
+        const arr = [];
+        for(let m = 0; m < 12; m++){
+            arr.push(generateMonth(year,m));
+        }
+        return arr;
+    }   
+}
 
 module.exports = calendarArray;
