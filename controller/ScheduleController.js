b1e58aec760a66d3ff42ce223e3941926b43ae91
asdominguez <asdominguez@loyola.edu>
2025-10-21 20:11:19 -0400

    completed schedule generation algorithm #8 #9 #10 #11

---
+++ controller/ScheduleController.js
@@ -0,0 +1,16 @@
+const dao = require('../model/SeasonScheduleDao');
+
+/**
+ * Store a new season schedule in the database.
+ * @param {*} req request object containing data
+ * @param {*} res response object used to send back
+ */
+const generateSeasonSchedule = async () => {
+    try {
+        const res = await fetch("/getAllTeams");
+        const teams = await res.json();
+        const 
+    } catch (err) {
+        res.status(500).send('Error registering');
+    }
+};
