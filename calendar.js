3aa4ed206fa6d83b3e78449ae3600e722a8a8cf4
chloemiranda <cmmiranda@loyola.edu>
2025-10-22 00:06:07 -0400

    revised calendar.js to have indindividual months

--- calendar.js
+++ calendar.js
@@ -1,57 +1,31 @@
-function calendarArray(year){
-    let arr = new Array(12);
-    for(let x = 0; x< arr.length;x++){
-        arr[x] = new Array(6)
-    }
 
-    for(let x = 0; x < arr.length; x++){
-        for(let y = 0; y < arr[x].length; y++) {
-            arr[x][y] = new Array(7);
-        }
-    }
 
-    for(let month = 0; month < arr.length; month++){
-        let startDayInWeek = new Date(year, month, 0). getDay() + 1;
-        let monthLong = new Date(year, month + 1,0).getDate() +1;
+function generateMonth (year, month){
+    const monthArr = Array.from({ length: 6}, () => new Array(7).fill(""));
+    const startDayInWeek = new Date(year, month, 1).getDay();
+    const monthLong = new Date(year, month, 1), getDay();
+    
+    let counter = 1;
+    let started = false;
 
-        console.log(monthLong)
-
-        let beforCount = 0;
-        let counter = 1;
-        let startCount = false;
-
-        for(let x = 0; x<arr[month].length; x++){
-            for(let y = 0; y<arr[month][x].length; y++){
-
-                if(beforCount == startDayInWeek){
-                    startCount = true;
-                }
-                else{
-                    beforCount++;
-                }
-
-                if(startCount == true){
-                    arr[month][x][y] = counter;
-                    counter++;
-                }
-
-                else{
-                    arr[month][x][y] = "";
-                }
-
-                if(counter > monthLong){
-                arr[month][x][y] = "";
-                }
+    for(let x = 0; x < 6; x++){
+        for(let y = 0; y < 7; y++){
+            if(!started && startDayInWeek){
+                started = true;
             }
 
-            
+            else if(started && counter <= monthLong){
+                monthArr[x][y] = counter;
+                counter++;
+            }
+
+            else{
+                monthArr[x][y] = "";
+            }
         }
-
     }
-
-    return arr;
-
-    
+    return monthArr
 }
+
 
 module.exports = calendarArray;
