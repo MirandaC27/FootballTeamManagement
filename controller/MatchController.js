5a237429afdbcd40db02a1ac47f1edc2a74e4ebb
chloemiranda <cmmiranda@loyola.edu>
2025-10-23 04:54:22 -0400

    finished MatchController

--- controller/MatchController.js
+++ controller/MatchController.js
@@ -22,12 +22,50 @@ const createNewMatch = async (req, res) => {
 
 
 const getAllMatches = async (req, res) => {
+   
+    try{
+        const matches = await dao.matchModel.find();
+        res.json(matches);
+    }
 
+    catch(err){
+        console.error('could not get matches', err);
+        res.status(500).send('Could not get matches');
+    }
 };
 
 
 const getCalendarData = async (req, res) => {
+    
+    try{
+        const year = parseInt(req.params.year);
+        const month = parseInt(req.params.month);
 
+        const monthData = calendarArray(year, month);
+        const matches = await dao.matchModel.find();
+
+        const matchDays = matches
+            
+            //get matches in requested month and year
+            .filter(m =>{
+                const date = new Date(m.matchDate);
+                return date.getMonth() === month && date.getFullYear() === year;
+            })
+            
+            //map each day to the month
+            .map(m => new Date(m.matchDate.getDate()));
+        res.json({
+            year: monthData.year,
+            monthName: monthData.monthName,
+            data: monthData.data,
+            matchDays: matchDays,
+        });
+    }
+
+    catch(err){
+        console.error('Error getting calendar data:', err);
+        res.status(500).send('Error getting calendar data');
+    }
 };
 
 module.exports = {
