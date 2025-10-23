aad30726012c54eebc4d113ea8cd7b5018fcba11
chloemiranda <cmmiranda@loyola.edu>
2025-10-23 04:12:34 -0400

    outline MatchController

---
+++ controller/MatchController.js
@@ -0,0 +1,16 @@
+const dao = require("../model/MatchDao");
+const calendarArray = require("../calendar-config");
+
+
+exports.createNewMatch = async function(req, res) {
+
+};
+
+exports.getAllMatches = async function(req, res) {
+
+};
+
+
+exports.getCalendarData = async function(req, res) {
+
+};
