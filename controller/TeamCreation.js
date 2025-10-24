4d3f934ada2f19333ed4b335d31f6979be5af0b9
JWehberg <wjwehberg@loyola.edu>
2025-10-23 21:41:46 -0400

    did the team creator .js. am going to do manage team now and then will add comments once fully done

---
+++ controller/TeamCreation.js
@@ -0,0 +1,14 @@
+const dao = require('../model/TeamDao');
+
+const createTeam = async (req, res) => {
+    try {
+        const { name, location, wins, losses, priorSeasonWins, priorSeasonLosses, playoffSeed } = req.body;
+        const newTeam = { name, location, wins, losses, priorSeasonWins, priorSeasonLosses, playoffSeed };
+        await dao.create(newTeam);
+        res.redirect('manager-teams.html?success=1');
+    } catch (err) {
+        res.status(500).send('error creating team');
+    }
+};
+
+module.exports = { teamCreation };
