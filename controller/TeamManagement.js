f68a517419cbc7a6faafd74a2d7a0f9d9852b2d5
JWehberg <wjwehberg@loyola.edu>
2025-10-23 22:51:30 -0400

    done the team management. am going to write the tests in a little bit

---
+++ controller/TeamManagement.js
@@ -0,0 +1,28 @@
+const dao = require('../model/MinorDao');
+
+const manageTeam = async (req, res) => {
+    try {
+        const { minorId, teamId, action } = req.body;
+        const minor = await dao.minorModel.findById(minorId);
+
+        if (!minor) {
+            return res.status(404).send('Minor not found');
+        }
+
+        if (action === 'add') {
+            minor.team_id = teamId;
+        } else if (action === 'remove') {
+            minor.team_id = null;
+        } else {
+            return res.status(400).send('Invalid action');
+        }
+
+        await minor.save();
+        res.redirect('manager-teams.html');
+    } catch (err) {
+        res.status(500).send('error managing team');
+    }
+};
+
+module.exports = { ManagementOfTeam };
+
