1c13119b561a70c3b071d5ec8bfa5dfaa5c2868c
nybutler <113071221+nybutler@users.noreply.github.com>
2025-10-26 12:04:46 -0400

    added html files

--- controller/MatchControllerNB.js
+++ controller/MatchControllerNB.js
@@ -1,6 +1,21 @@
-const matchviewDao = require('../model/MatchViewDao');
+const matchDao = require('../model/MatchDaoNB');
 const teamDao = require('../model/TeamDao');
 
+
+/**
+ * Get all matches from the database.
+ * @param {*} req request object containing data
+ * @param {*} res response object to send back
+ */
+const getAllMatches = async (req, res) => {
+    try{
+        const matches = await matchDao.matchModel.find();
+        res.json(matches);
+    }
+    catch (err){
+        res.status(500).send('Error finding matches.');
+    }
+};
 
 
 /**
@@ -10,8 +25,35 @@ const teamDao = require('../model/TeamDao');
  */
 const getMatchDetails = async (req, res) => {
     try{
-        const matchID = req.params.id;
-        
+        const matchId = req.params.id;
+        const match = await matchDao.findById(matchId).lean();
+
+        if(!match){
+            return res.status(404).json({message:'Invalid Match.'}); 
+        }
+
+        const homeTeam = await teamDao.findById(match.homeTeam).lean();
+        const awayTeam = await teamDao.findById(match.awayTeam).lean();
+
+        const matchData = {
+            matchDatetime: match.matchDatetime,
+            matchLocation: match.matchLocation,
+            matchStatus: match.matchStatus,
+            homeScore: match.homeScore,
+            awayScore: match.awayScore,
+
+            homeTeam: match.homeTeam,
+            awayTeam: match.awayTeam
+        };
+
+        res.status(200).json(matchData);
     }
-    catch{}
+    catch (err){
+        res.status(500).send('Error loading match.');
+    }
 };
+
+module.exports = {
+    getAllMatches,
+    getMatchDetails
+}
