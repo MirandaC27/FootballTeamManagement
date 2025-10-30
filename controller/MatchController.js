152c15c0ffbe4e201da6607d9ac25b8e2b6a0491
chloemiranda <cmmiranda@loyola.edu>
2025-10-30 13:29:14 -0400

    redid matchController.js (#L25, #L37, #L38)

--- controller/MatchController.js
+++ controller/MatchController.js
@@ -1,6 +1,63 @@
 const matchDao = require('../model/MatchDaoNB');
 const teamDao = require('../model/TeamDao');
 
+/*
+    homeTeam
+    awayTeam
+    homeScore
+    awayScore
+    matchDatetime
+    matchLocation
+    matchStatus
+*/
+
+/**
+ * Create a new match and register it in the database.
+ * @param {*} req request object containing data
+ * @param {*} res response object used to send back
+ */
+const createNewMatch = async (req, res) => {
+    try{
+        const{ homeTeam, awayTeam, homeScore, awayScore, matchDatetime, matchLocation, matchStatus } = req.body;
+        
+        if(!homeTeam || !awayTeam || !homeScore || !awayScore || !matchDatetime || !matchLocation || !matchStatus){
+            return res.status(400).send('missing attribute');
+        }
+
+        const newMatch = new dao.matchModel({ homeTeam, awayTeam, homeScore, awayScore, matchDatetime, matchLocation, matchStatus});
+        await newMatch.save();
+        res.status(200).json({ message: "Match added successfully" });
+    }
+
+    catch(err){
+        console.error('Could not create match:', err);
+        res.status(500).send('Could not create match');
+    }
+};
+
+/**
+ * delete an existing match and register it in the database.
+ * @param {*} req request object containing data
+ * @param {*} res response object used to send back
+ */
+const deleteMatch = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleted = await dao.matchModel.findByIdAndDelete(id);
+
+    if (!deleted) {
+      return res.status(404).json({ message: "Match not found" });
+    }
+
+    res.json({ message: "Match deleted successfully" });
+  } 
+  
+  catch (err) {
+    console.error("Match deleting event:", err);
+    res.status(500).json({ message: "Server error while deleting match" });
+  }
+};
+
 
 /**
  * Get all matches from the database.
