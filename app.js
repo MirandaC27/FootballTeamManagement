0dc5211593cdba3d80b233c532c328202c329bf7
lesliekim1 <lskim@loyola.edu>
2025-11-02 00:59:27 -0400

    remove 1 comment #32

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const session = require('express-session');
-const multer = require('multer'); // required for file upload
+const multer = require('multer');
 const path = require('path');
 const app = express();
 
