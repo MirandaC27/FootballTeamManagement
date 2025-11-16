a75af13cc7c9b7b243aae06853085e7f597f377a
lesliekim1 <lskim@loyola.edu>
2025-11-16 00:18:15 -0500

    added socket.io and basic live chat (doesn't save to database yet) #50

--- server.js
+++ server.js
@@ -6,6 +6,22 @@ dbcon.connect(); // Connect to MongoDB when starting the server.
 
 const ExpressApp = require('./app');
 
-ExpressApp.listen(process.env.PORT,process.env.HOSTNAME,function(){ // Listen to client requests in hostname:port
+const server = ExpressApp.listen(process.env.PORT, process.env.HOSTNAME, function () { // Listen to client requests in hostname:port
     console.log(`Server Running on ${process.env.HOSTNAME}:${process.env.PORT}...`);
+});
+
+// socket.io for live chat
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    console.log('connected to server');
+
+    socket.on('message', (msg) => {
+        io.emit('message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('disconnected from server');
+    });
 });
